7068bb429641beaaee80c6b0a069b7531cd023a9
Bhogendra Kumar Das <bhogendrakumardas123@gmail.com>
2026-07-21 08:11:17 +0545

    modify the code of recaptcha middleware file to solve the error

---
+++ backend/web-API-foodapp/src/middlewares/recaptcha.middleware.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { Request, Response, NextFunction } from "express";
+
+export const verifyRecaptcha = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { captchaToken } = req.body;
+
+  if (!captchaToken) {
+    return res.status(400).json({
+      success: false,
+      message: "CAPTCHA token is missing",
+    });
+  }
+
+  try {
+    const response = await axios.post(
+      "https://www.google.com/recaptcha/api/siteverify",
+      null,
+      {
+        params: {
+          secret: process.env.RECAPTCHA_SECRET_KEY,
+          response: captchaToken,
+        },
+      }
+    );
+
+    if (!response.data.success) {
+      return res.status(400).json({
+        success: false,
+        message: "CAPTCHA verification failed",
+      });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "CAPTCHA verification error",
+    });
+  }
+};
