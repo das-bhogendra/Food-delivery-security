107d09c2f5a2c7c2dbd2f8ce443609579536af12
Bhogendra Kumar Das <bhogendrakumardas123@gmail.com>
2026-07-21 08:05:29 +0545

    add recaptcha middleware file

--- backend/web-API-foodapp/src/routes/auth.route.ts
+++ backend/web-API-foodapp/src/routes/auth.route.ts
@@ -4,12 +4,13 @@ import { authorizedMiddleware } from "../middlewares/authorization.middleware";
 import { uploads } from "../middlewares/upload.middleware";
 import { AuthController } from "../controllers/auth.controller";
 import { loginRateLimiter } from "../middlewares/rateLimit.middleware";
+import { verifyRecaptcha } from "../middlewares/recaptcha.middleware";
 
 const router = Router();
 const authController = new AuthController();
 
 router.post("/register", authController.register);
-router.post("/login", loginRateLimiter, authController.login);
+router.post("/login", loginRateLimiter,verifyRecaptcha, authController.login);
 
 // ✅ ADD THIS
 router.get("/whoami", authorizedMiddleware, authController.getProfile);
